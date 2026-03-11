6d3db244e694f3e4117edb90fd6cb89bde489cb5
Daniel <danielmcgrath91@gmail.com>
2026-03-10 22:19:45 -0700

    Add AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4335841189379651"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
